6185f05867d9896e8ff0024cc771ed2a174c37ab
limhhyeon <honghyeon1007@gmail.com>
2025-01-26 13:04:05 +0900

    feat: start project

---
+++ src/main/java/com/github/farmplus/FarmPlusApplication.java
@@ -0,0 +1,13 @@
+package com.github.farmplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmPlusApplication.class, args);
+	}
+
+}
