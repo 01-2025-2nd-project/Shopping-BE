a7f7b95561687729a366c05197e9aabd34ead0da
yj267 <uujin1023@daum.net>
2025-01-30 10:31:54 +0900

    feat: SignController 내 return문 변경

--- src/main/java/com/github/farmplus/web/controller/SignController.java
+++ src/main/java/com/github/farmplus/web/controller/SignController.java
@@ -4,6 +4,7 @@ import com.github.farmplus.repository.userDetails.CustomUserDetails;
 import com.github.farmplus.service.AuthService;
 import com.github.farmplus.web.dto.auth.Login;
 import com.github.farmplus.web.dto.auth.SignUp;
+import com.github.farmplus.web.dto.base.ResponseDto;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -20,23 +21,32 @@ public class SignController {
     private final AuthService authService;
 
     @PostMapping(value = "/sign-up")
-    public String register(@RequestBody SignUp signUpRequest){
+    public ResponseDto register(@RequestBody SignUp signUpRequest){
         boolean isSuccess = authService.signUp(signUpRequest);
-        return isSuccess ? "회원가입 성공하였습니다." : "회원가입 실패하였습니다.";
+        if (isSuccess) {
+            return new ResponseDto(HttpStatus.OK.value(), "회원가입에 성공하였습니다.");
+        } else {
+            return new ResponseDto(HttpStatus.BAD_REQUEST.value(), "회원가입에 실패하였습니다.");
+        }
     }
 
     @PostMapping(value = "/login")
-    public String login(@RequestBody Login loginRequest, HttpServletResponse httpServletResponse){
+    public ResponseDto login(@RequestBody Login loginRequest, HttpServletResponse httpServletResponse) {
         String token = authService.login(loginRequest);
-        httpServletResponse.setHeader("Authorization", "Bearer "+token);
-        return "로그인에 성공하였습니다.";
+        if (token != null && !token.isEmpty()) {
+            httpServletResponse.setHeader("Authorization", "Bearer " + token);
+            return new ResponseDto(HttpStatus.OK.value(), "로그인에 성공하였습니다.", token);
+        } else {
+            return new ResponseDto(HttpStatus.UNAUTHORIZED.value(), "로그인에 실패하였습니다.");
+        }
     }
 
 
     @GetMapping("/test")
-    public String test(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
-        // CustomUserDetails가 null일 수 있으므로 예외 처리
-        return customUserDetails.getEmail();
+    public ResponseDto test(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        if (customUserDetails == null) {
+            return new ResponseDto(HttpStatus.UNAUTHORIZED.value(), "인증 정보가 없습니다.");
+        }
+        return new ResponseDto(HttpStatus.OK.value(), "요청이 성공했습니다.", customUserDetails.getEmail());
     }
-
 }
